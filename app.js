3ada10475095ef69ecd95ba56d11d5c0d7daedf2
K_dev11 <kkeobangsy@gmail.com>
2023-05-31 05:56:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,35 +4,39 @@ const submit = document.querySelector(".submit-btn");
 var list = document.getElementById("list");
 var clearForm = document.querySelectorAll("input");
 var clear = document.querySelector(".erase");
+var cost = document.querySelector("cost");
+var mass = document.getElementById("dropList")
 submit.addEventListener('click', addText);
 clear.addEventListener("click", clearList);
+mass.addEventListener("click", drop);
 
-const plus = "add";
-const minus = "subtract";
+
 
 function addText() {
   const title = document.getElementById("title").value;
 
     const item = document.createElement("h2")
-    item.textContent = document.querySelector("#title").value + " " 
-    + document.querySelector("#text").value;
+    item.textContent = document.querySelector("#title").value ;
+
+    
 
     item.addEventListener("click", () => {
       if(item.style.color != "red")
-        item.addEventListener("click", () => {
-            if(item.style.textDecoration != "line-through") 
-              item.style.textDecoration = "line-through";
-              else
-              item.style.textDecoration = "none";
-          })
-      else
-
-          item.style.color = "black";
+      item.style.color = "red"
+        else 
+           item.style.color = "black"
+          
+  
     })
-
-
+    item.addEventListener("click", () => {
+      if(item.style.textDecoration != "line-through") 
+      item.style.textDecoration = "line-through";
+      else 
+      item.style.textDecoration = "none"; 
+    })
+    
+    //drop();
     document.querySelector("#list").appendChild(item);
-    document.querySelector("#title").value = "";
 
     clearForm.forEach(singleInput => singleInput.value = innerHTML = "" );
 
@@ -40,7 +44,29 @@ function addText() {
   
 }
 
+
+function drop() {
+      list.innerHTML = mass.value;
+
+  console.log(list.innerHTML);
+
+}
+
+
+function increase() {
+    var upButton = document.getElementById("");
+    for (let i = 0; i < 10; i++){
+      
+    }
+
+}
+
+function decrease() {
+
+}
+
 function clearList() {
   document.querySelector("#list").textContent = " ";
 }
+
 
