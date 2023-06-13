aab26c193040538f2485dd9964b3ce5eee8abe92
K_dev11 <kkeobangsy@gmail.com>
2023-06-13 01:08:56 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ var list = document.getElementById("list");
 var clearForm = document.querySelectorAll("input");
 var clear = document.querySelector(".erase");
 var cost = document.querySelector("cost");
-var mass = document.getElementById("dropList")
+const dropdownTitle = document.querySelector(".dropdown .title");
+const dropdownOptions = document.querySelectorAll(".dropdown .option");
+//var measure = document.querySelector("#dropList");
+dropdownOptions.forEach(option => option.addEventListener("click", handleOptionSelected));
+document.querySelector(".dropdown .title").addEventListener("change", handleTitleChange);
 submit.addEventListener('click', addText);
 clear.addEventListener("click", clearList);
-mass.addEventListener("click", drop);
+dropdownTitle.addEventListener("click", toggleMenuDisplay);
+//measure.addEventListener("click", drop);
 
 
 
@@ -35,7 +40,7 @@ function addText() {
       item.style.textDecoration = "none"; 
     })
     
-    //drop();
+    
     document.querySelector("#list").appendChild(item);
 
     clearForm.forEach(singleInput => singleInput.value = innerHTML = "" );
@@ -44,12 +49,59 @@ function addText() {
   
 }
 
+function toggleClass(elem, className) {
+  if (elem.className.indexOf(className) !== -1){
+    elem.className = elem.className.replace(className, ' ');
+  } else {
+    elem.className = elem.className.replace(/\s+/g, ' ') + ' ' + className;
+  }
 
-function drop() {
-      list.innerHTML = mass.value;
+  return elem;
 
-  console.log(list.innerHTML);
+}
 
+function toggleDisplay(elem) {
+  const curDisplayStyle = elem.style.display;
+
+  if (curDisplayStyle === 'none' || curDisplayStyle === '') {
+    elem.style.display = 'block';
+  } else {
+    elem.style.display = 'none';
+  }
+}
+
+function toggleMenuDisplay(e){
+      const dropdown = e.currentTarget.parentNode;
+      const menu = dropdown.querySelector('.menu');
+      const icon = dropdown.querySelector('.fa-angle-right');
+    
+  toggleClass(menu, 'hide');
+  toggleClass(icon, 'rotate-90');
+
+}
+
+function handleOptionSelected(e) {
+  toggleClass(e.target.parentNode, 'hide');
+
+    const id = e.target.id;
+    const newValue = e.target.textContent + ' ';
+    const titleElem = document.querySelector('.dropdown .title');
+    const icon = document.querySelector('.dropdown .title .fa');
+
+    titleElem.textContent = newValue;
+    titleElem.appendChild(icon);
+
+  document.querySelector('.dropdown .title').dispatchEvent(new Event('change'));
+  setTimeout(() => toggleClass(icon, 'rotate-90', 0));
+
+}
+
+function handleTitleChange(e) {
+  const result = document.getElementById('list1');
+  const size = e.target.textContent
+
+  result.innerHTML = size;
+  
 }
 
 
